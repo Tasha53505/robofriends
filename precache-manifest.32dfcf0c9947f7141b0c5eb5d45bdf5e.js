dd61d5db0d47e39e0372410bf9cd1aea39b9f23c
Tasha <ncarr63@gmail.com>
2022-01-07 04:05:26 +1300

    Updates

---
+++ precache-manifest.32dfcf0c9947f7141b0c5eb5d45bdf5e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f62eb06ec330fc067475",
+    "url": "/robofriends/static/css/main.6848056f.chunk.css"
+  },
+  {
+    "revision": "f62eb06ec330fc067475",
+    "url": "/robofriends/static/js/main.f62eb06e.chunk.js"
+  },
+  {
+    "revision": "0bf54a1201dab9a9b136",
+    "url": "/robofriends/static/js/1.0bf54a12.chunk.js"
+  },
+  {
+    "revision": "c3834c2f19d47d5601e9",
+    "url": "/robofriends/static/css/2.f37427dd.chunk.css"
+  },
+  {
+    "revision": "c3834c2f19d47d5601e9",
+    "url": "/robofriends/static/js/2.c3834c2f.chunk.js"
+  },
+  {
+    "revision": "d9078623073d367ccac9",
+    "url": "/robofriends/static/js/runtime~main.d9078623.js"
+  },
+  {
+    "revision": "ec97511a3e13f31b031918320a21bc68",
+    "url": "/robofriends/static/media/Dubstep Dungeons.ec97511a.woff"
+  },
+  {
+    "revision": "49811ee415284ad8cca0e441d6db25ff",
+    "url": "/robofriends/index.html"
+  }
+];
